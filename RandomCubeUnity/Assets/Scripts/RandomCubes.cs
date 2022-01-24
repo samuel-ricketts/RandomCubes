de68e9d10d7b129b7adcc42fe528fbb670a40f09
samuel-ricketts <78981392+samuel-ricketts@users.noreply.github.com>
2022-01-24 16:35:15 -0600

    -Created cube prefab

---
+++ RandomCubeUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,27 @@
+/****
+ * Created by: Sammy Ricketts
+ * Date Created: Jan 24, 2022
+ * 
+ * Last Edited by: NA
+ * Last Edited: Jan 24, 2022
+ * 
+ * Description: Spawns multiple cube prefabs into scene.
+ ****/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
